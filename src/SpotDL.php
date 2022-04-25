168928c18d4e00b07dd1a4a907d611a0bf410505
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2022-04-25 09:32:28 -0400

    Add files via upload

--- src/SpotDL.php
+++ src/SpotDL.php
@@ -1,1 +1,15 @@
+<?PHP
+    
+namespace SpotDL;
 
+class SpotDL{
+
+  private cmd;
+  
+  public function download($spotifyUrl){
+    $cmd = 'python /app/spotdl '.$spotifyUrl;
+    exec($cmd,$output,$rescode);
+    
+  }
+
+}
