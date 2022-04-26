af68c2a106d83033308a5c36568a7c98da7f2032
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2022-04-26 06:06:05 -0400

    Add files via upload

--- src/SpotDL.php
+++ src/SpotDL.php
@@ -6,8 +6,15 @@ class SpotDL{
     $cmd = 'python /app/spotdl/__main__.py '.$spotifyUrl.' 2>&1';
     echo $cmd;
     exec($cmd,$output,$rescode);
-    var_dump($output);
-    echo $rescode;
+    foreach($output as $line){
+      //echo $line.'\n';
+    }
+    return $this->parseMusicName($output[1]);
   }
 
+  private function parseMusicName($str){
+    preg_match('#Searching YouTube music for \"(.*?)\"#i',$str,$res);
+    $name = $res[1][0].'.mp3';
+    return $name;
+  }
 }
