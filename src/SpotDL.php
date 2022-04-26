2bf14c0010a43fbf5f84778da45c4d5f7fc843db
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2022-04-26 07:45:18 -0400

    Add files via upload

--- src/SpotDL.php
+++ src/SpotDL.php
@@ -7,13 +7,14 @@ class SpotDL{
     echo $cmd;
     exec($cmd,$output,$rescode);
     foreach($output as $line){
-      //echo $line.'\n';
+      echo $line.'\n';
     }
     return $this->parseMusicName($output[1]);
   }
 
   private function parseMusicName($str){
     preg_match('#Searching YouTube music for "(.*?)"#i',$str,$res);
+    var_dump($res);
     $name = $res[1][0].'.mp3';
     return $name;
   }
