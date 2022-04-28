181df93e199ff9f06372d219d221239acacb08e7
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2022-04-28 09:22:25 -0400

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -2,30 +2,37 @@
 
 ini_set('error_reporting', E_ALL ^ E_NOTICE); 
 ini_set('display_errors', 1);
-$q = isset($_GET['q']) ? urlencode($_GET['q']) : null;
-$type = 'track';
 
-$url = 'https://api.spotify.com/v1/search?q='. $q .'&type='. $type .'&market=ES&limit=10';
-$access_token = get_access_token();
-$headers = [
-  "Accept: application/json",
-  "Authorization: Bearer $access_token"
-];
-var_dump($headers);
-var_dump($url);
-$c = curl_init($url);
-curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($c, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
-$res = curl_exec($c);
-$json = json_decode($res, true);
+$q = isset($_GET['q']) ? $_GET['q'] : null;
+if(isset($q)){
+  $json = get_spotify_json($q);
+}
+
+function get_spotify_json($q){
+  $url = 'https://api.spotify.com/v1/search?q='.urlencode($q).'&type=track&market=ES&limit=10';
+  $access_token = get_access_token();
+  $headers = [
+    "Accept: application/json",
+    "Authorization: Bearer $access_token"
+  ];
+  $c = curl_init($url);
+  curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($c, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
+  $res = curl_exec($c);
+  $json = json_decode($res, true);
+  return $json;
+}
 
 function get_access_token(){
   $url = 'https://accounts.spotify.com/api/token';
   $postdata = 'grant_type=client_credentials';
+  $client_key    = getenv('SPOTIFY_KEY');
+  $client_secret = getenv('SPOTIFY_SECRET');
+  $basic_auth_string   = base64_encode($client_key .':'. $client_secret);
   $headers = [
     "Content-Type: application/x-www-form-urlencoded",
-    "Authorization: Basic ZmYwM2I5YjM5MWEwNGVlYWI4YzFmYWI3YWQ1NTUwZTM6MjE5OTMxZTY5MjM5NGI0Yjk4ZjVlZDBhNDY3ODViZmE="
+    "Authorization: Basic $basic_auth_string"
   ];
   $c = curl_init($url);
   curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
@@ -81,7 +88,7 @@ function get_access_token(){
 if(isset($q)){
 foreach($json['tracks']['items'] as $item){
   echo '<div class="opts">';
-    echo '<img width="'. $item['album']['images'][2]['width'] .'" height="'. $item['album']['images'][2]['height'] .'" src="'. $item['album']['images'][2]['url'] .'">';
+    echo '<img width="'. $item['album']['images'][1]['width'] .'" height="'. $item['album']['images'][1]['height'] .'" src="'. $item['album']['images'][1]['url'] .'">';
     echo '<p>'. $item['name'] .'</p>';
     if(isset($item['preview_url'])){
       echo '<audio controls>';
