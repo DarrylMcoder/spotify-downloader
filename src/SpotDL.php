aded6e0c560c25b284476de5b7ecc34f4f19c07b
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2022-04-29 12:56:03 -0400

    Add files via upload

--- src/SpotDL.php
+++ src/SpotDL.php
@@ -22,7 +22,9 @@ class SpotDL{
     unlink('names.txt');
     file_put_contents('names.txt', 'file \'watermark.mp3\''. PHP_EOL .'file \''. $filename .'\'');
     //rename($filename, $infile);
-    $cmd = 'ffmpeg -f concat -safe 0 -i names.txt -c copy "'. $tempfile .'" 2>&1';
+   // $cmd = 'ffmpeg -f concat -safe 0 -i names.txt -c copy "'. $tempfile .'" 2>&1';
+
+    $cmd = 'ffmpeg -f concat -i names.txt -i \''. $filename .'\' -map_metadata 1 -id3v2_version 3 -write_id3v1 1 -c copy "'. $tempfile .'" 2>&1';
     echo $cmd;
     exec($cmd, $output, $rescode);
     var_dump($output);
