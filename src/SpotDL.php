2dde9fc088f04856e2548fce000adef599f34076
Darryl Martin <80832451+DarrylMcoder@users.noreply.github.com>
2022-05-17 07:50:59 -0400

    Add files via upload

--- src/SpotDL.php
+++ src/SpotDL.php
@@ -4,9 +4,14 @@ class SpotDL{
   
   public function download($spotifyUrl){
     $spotifyUrl = escapeshellcmd($spotifyUrl);
-    $cmd = 'python /app/spotdl/__main__.py '.$spotifyUrl.' 2>&1';
-    exec($cmd,$output,$rescode);
-    return $this->parseMusicName($output[1]);
+    $cmd = 'python /app/spotdl/__main__.py '.$spotifyUrl.' -f /dev/stdout 1>&2';
+    $fp = popen($cmd,"r");
+    while(!feof($fp)){
+      echo fgets($fp, 4096);
+    }
+    pclose($fp);
+    
+    //return $this->parseMusicName($output[1]);
   }
 
   private function parseMusicName($str){
@@ -17,22 +22,6 @@ class SpotDL{
   }
   
   public function watermark($filename){
-    $filename = escapeshellcmd($filename);
-    $infile = preg_replace("#[^\w]#i", "_", $filename) .'.mp3';
-    $outfile = '_'. $infile;
-    unlink('names.txt');
-    file_put_contents('names.txt', 'file \''.  $infile.'\''. PHP_EOL .'file \''. $infile .'\'');
-    rename($filename, $infile);
-   // $cmd = 'ffmpeg -f concat -safe 0 -i names.txt -c copy "'. $tempfile .'" 2>&1';
-
-   // $cmd = 'ffmpeg -f concat -i names.txt -i \''. $infile .'\' -map_metadata 0:1 -id3v2_version 3 -write_id3v1 1 -c:a copy "'. $outfile .'" 2>&1';
-    
-    $cmd = "ffmpeg -i watermark.mp3 -i $infile -map_metadata 1 -filter_complex amix=inputs=2:duration=longest $outfile 2>&1";
-    echo $cmd;
-    exec($cmd, $output, $rescode);
-    rename($outfile, $filename);
-    var_dump($output);
-    echo $rescode;
-    return $filename;
+    //not implemented
   }
 }
