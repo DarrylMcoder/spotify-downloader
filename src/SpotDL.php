fc5b5acd92fab63f6499fec53d56cea89d5a13e8
Darryl Martin <80832451+DarrylMcoder@users.noreply.github.com>
2022-05-18 10:06:15 -0400

    Add files via upload

--- src/SpotDL.php
+++ src/SpotDL.php
@@ -4,14 +4,9 @@ class SpotDL{
   
   public function download($spotifyUrl){
     $spotifyUrl = escapeshellcmd($spotifyUrl);
-    $cmd = 'python /app/spotdl/__main__.py -- '.$spotifyUrl.' --output-file - 1>&2';
-    $fp = popen($cmd,"r");
-    while(!feof($fp)){
-      echo fgets($fp, 4096);
-    }
-    pclose($fp);
-    
-    //return $this->parseMusicName($output[1]);
+    $cmd = 'python /app/spotdl/__main__.py '.$spotifyUrl.' 1>&2';
+    shell_exec($cmd,$output,$rescode);
+    return $this->parseMusicName($output[1]);
   }
 
   private function parseMusicName($str){
