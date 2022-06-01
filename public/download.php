b63874b3874e7fd7546e028708b7a722c383155d
Darryl Martin <80832451+DarrylMcoder@users.noreply.github.com>
2022-06-01 10:57:58 -0400

    Add files via upload

--- public/download.php
+++ public/download.php
@@ -1,5 +1,5 @@
 <?PHP
-    
+
 ini_set('error_reporting', E_ALL ^ E_NOTICE); 
 ini_set('display_errors', 1);
 $name = $_GET['name'];
@@ -8,12 +8,5 @@ $img_url = $_GET['img_url'];
 $preview_url = $_GET['preview_url'];
 $downloads = 1;
 $timestamp = time();
-header("Location: download2.php?url=" . $url);
-flush();
-
-include('./config.php');
-$sql = "INSERT INTO songs(artist, name, url, img_url, preview_url, downloads, timestamp) VALUES(?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE downloads = downloads + 1, timestamp = ?";
-$stmt = $mysqli->prepare($sql);
-$stmt->bind_param('sssssiii',$artist, $name, $url, $img_url, $preview_url, $downloads, $timestamp, $timestamp);
-$stmt->execute();
-echo $mysqli->error;
+$filename = $spotdl->download($url);
+echo "<head><script>location.href='http://yt.app.darrylmcoder.com/download.php?n=". $filename ."&url=http://spotdl.darrylmcoder.com/". $filename ."'</script></head><body></body>";
