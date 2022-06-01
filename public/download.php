29c00e89a7cfd4a8086dfb6f55e856d65919020b
Darryl Martin <80832451+DarrylMcoder@users.noreply.github.com>
2022-06-01 17:44:07 -0400

    Add files via upload

--- public/download.php
+++ public/download.php
@@ -1,12 +1,33 @@
 <?PHP
-
+    
 ini_set('error_reporting', E_ALL ^ E_NOTICE); 
 ini_set('display_errors', 1);
+require('../src/SpotDL.php');
+require('../src/YouTubeStreamer.php');
+require('../src/VideoSaver.php');
+use \YouTube\YouTubeStreamer;
+use \YouTube\VideoSaver;
+
+$artist = '';
 $name = $_GET['name'];
 $url = $_GET['url'];
 $img_url = $_GET['img_url'];
 $preview_url = $_GET['preview_url'];
 $downloads = 1;
 $timestamp = time();
+
+$spotdl = new SpotDL();
 $filename = $spotdl->download($url);
-echo "<head><script>location.href='http://yt.app.darrylmcoder.com/download.php?n=". $filename ."&url=http://spotdl.darrylmcoder.com/". $filename ."'</script></head><body></body>";
+
+
+$url = 'http://spotdl.darrylmcoder.com/'.$filename;
+$downloader = new VideoSaver();
+$downloader->setDownloadedFileName($filename);
+$downloader->download($url);
+
+include('./config.php');
+$sql = "INSERT INTO songs(artist, name, url, img_url, preview_url, downloads, timestamp) VALUES(?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE downloads = downloads + 1, timestamp = ?";
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param('sssssiii',$artist, $name, $url, $img_url, $preview_url, $downloads, $timestamp, $timestamp);
+$stmt->execute();
+//echo $mysqli->error;
